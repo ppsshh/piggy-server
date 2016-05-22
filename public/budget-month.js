73a8de1c0c5207e3c64434333f3b6af3697017e9
Psh <dev@mtbdx.com>
2016-05-22 09:47:56 +0300

    Adds permanent highlighting (just click on "expense category" or any "expense row" to turn on/off highlighting)

--- public/budget-month.js
+++ public/budget-month.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+  var fixedHighlight = function(expenseType){
+    expenseType = expenseType + "-fixed";
+    $('#expenses-table').toggleClass(expenseType);
+    return false;
+  };
   $('.expense-button').click(function(event){
     event.preventDefault();
-    var myClass = $(this).attr("data-name");
-    $('#expenses-table').toggleClass(myClass);
-    return false;
+    fixedHighlight( $(this).attr("data-name") );
+  });
+  $('.expense-row').click(function(){
+    fixedHighlight( $(this).attr("data-name") );
   });
   $('.expense-row').hover(function(){
     var myClass = $(this).attr("data-name");

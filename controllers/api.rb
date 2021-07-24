ed4650fe55c1bee8c6636ca6e7838329ed003e52
Psh <dev@mtbdx.com>
2021-07-24 13:22:20 +0900

    API: Add 'globals' endpoint

--- controllers/api.rb
+++ controllers/api.rb
@@ -1,5 +1,6 @@
 paths \
-    api_month:  '/api/month/:year/:month'
+    api_month: '/api/month/:year/:month',
+    globals: '/api/globals'
 
 get :api_month do
   protect!
@@ -11,6 +12,15 @@ get :api_month do
     operations: BudgetRecord.where(date: date_start...date_end, purse: 0).each_with_object({}) do |br, acc|
       (acc[br.date] ||= []) << br
     end,
-    savings: BudgetRecord.where(date: date_start...date_end, purse: 1),
+    savings: BudgetRecord.where(date: date_start...date_end, purse: [1, 2]),
+  }.to_json
+end
+
+get :globals do
+  protect!
+
+  {
+    tags: Tag.all.index_by(&:id),
+    currencies: Currency.all.index_by(&:id),
   }.to_json
 end

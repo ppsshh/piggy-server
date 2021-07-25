72f7b1df03c5ab36fde0ef0444fe48b3f5e4f185
Psh <dev@mtbdx.com>
2021-07-25 12:13:31 +0900

    API: Add seller autocompletion endpoint

---
+++ controllers/autocomplete.rb
@@ -0,0 +1,12 @@
+paths \
+    api_autocomplete_shop: '/api/autocomplete/shop'
+
+post :api_autocomplete_shop do
+  protect!
+
+  tr_query = params[:query].downcase.tr("qwertyuiop[]asdfghjkl;'zxcvbnm,.`", "йцукенгшщзхъфывапролджэячсмитьбюё")
+  items = BudgetRecord.where('shop ILIKE ? OR shop ILIKE ?', "%#{params[:query]}%", "%#{tr_query}%")
+    .order(shop: :asc).select(:shop).distinct.limit(20).pluck(:shop)
+
+  items.to_json
+end
